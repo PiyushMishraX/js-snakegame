b2ae3f686ee9fe0d421ea2c0858df0a9c887db07
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-04-19 23:16:53 +0530

    sfx

--- final_script.js
+++ final_script.js
@@ -1,5 +1,8 @@
 const board = document.querySelector(".board");
 
+// sounds
+
+
 const startButton = document.querySelector(".btn-start");
 const modal = document.querySelector(".modal");
 const startGameModal = document.querySelector(".start-game");
@@ -62,6 +65,7 @@ function render() {
 
   blocks[`${food.x}-${food.y}`].classList.add("food");
 
+
   if (direction === "left") {
     head = { x: snake[0].x, y: snake[0].y - 1 };
   } else if (direction === "right") {
@@ -85,12 +89,21 @@ function render() {
   }
 
   if (head.x == food.x && head.y == food.y) {
+
+    // Eating audio
+    // audioEat.currentTime = 0;
+    const audioEat = new Audio('/sfx/music_food.mp3');
+    audioEat.play()
+
+
     blocks[`${food.x}-${food.y}`].classList.remove("food");
     food = {
       x: Math.floor(Math.random() * rows),
       y: Math.floor(Math.random() * cols),
     };
     blocks[`${food.x}-${food.y}`].classList.add("food");
+
+    
 
     snake.unshift(head);
 
