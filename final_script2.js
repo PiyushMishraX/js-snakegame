8a8cd3178a8bddae37e529e2e853fa73015263e6
Piyushmishrax <piyushmishra.connect@gmail.com>
2026-05-12 21:43:10 +0530

    final commit

--- final_script2.js
+++ final_script2.js
@@ -1,6 +1,5 @@
 const board = document.querySelector(".board");
 
-
 const startButton = document.querySelector(".btn-start");
 const modal = document.querySelector(".modal");
 const startGameModal = document.querySelector(".start-game");
@@ -21,11 +20,9 @@ let highScore = localStorage.getItem("highScore") || 0;
 let score = 0;
 let time = `00-00`;
 
-
-const audioEat = new Audio('/sfx/music_food.mp3');
-audioEat.preload = 'auto';
-audioEat.load(); 
-
+const audioEat = new Audio("/sfx/music_food.mp3");
+audioEat.preload = "auto";
+audioEat.load();
 
 highScoreElement.innerHTML = highScore;
 
@@ -40,17 +37,17 @@ let timerIntervalId = null;
 //   y: Math.floor(Math.random() * cols),
 // };
 
-// let food = generateFood() // temporal dead zone because snake is defined below here and generate food use snake array 
+// let food = generateFood() // temporal dead zone because snake is defined below here and generate food use snake array
 // const blocks = [];
 
 let snake = [
-    {
-        x: 1,
-        y: 3,
-    },
+  {
+    x: 1,
+    y: 3,
+  },
 ];
 
-let food = generateFood() // temporal dead zone because snake is defined below here and generate food use snake array // now have putten it below snake
+let food = generateFood(); // temporal dead zone because snake is defined below here and generate food use snake array // now have putten it below snake
 const blocks = [];
 
 let direction = "down";
@@ -70,7 +67,6 @@ function render() {
 
   blocks[`${food.x}-${food.y}`].classList.add("food");
 
-
   if (direction === "left") {
     head = { x: snake[0].x, y: snake[0].y - 1 };
   } else if (direction === "right") {
@@ -81,36 +77,38 @@ function render() {
     head = { x: snake[0].x - 1, y: snake[0].y };
   }
 
-  const isSelfCollision = snake.some( (segment) =>{
+  const isSelfCollision = snake.some((segment) => {
     return segment.x === head.x && segment.y === head.y;
   });
 
-  if (head.x < 0 || head.x >= rows || head.y < 0 || head.y >= cols || isSelfCollision) {
+  if (
+    head.x < 0 ||
+    head.x >= rows ||
+    head.y < 0 ||
+    head.y >= cols ||
+    isSelfCollision
+  ) {
     clearInterval(intervalId);
 
     modal.style.display = "flex";
     startGameModal.style.display = "none";
-    gameOverScoreCurrent.innerHTML= scoreElement.innerHTML
-    gameOverScoreHigh.innerHTML= highScoreElement.innerHTML
+    gameOverScoreCurrent.innerHTML = scoreElement.innerHTML;
+    gameOverScoreHigh.innerHTML = highScoreElement.innerHTML;
     gameOverModal.style.display = "flex";
 
     return;
   }
 
   if (head.x == food.x && head.y == food.y) {
-
-    audioEat.play()
-
+    audioEat.play();
 
     blocks[`${food.x}-${food.y}`].classList.remove("food");
     // food = {
     //   x: Math.floor(Math.random() * rows),
     //   y: Math.floor(Math.random() * cols),
     // };
-    food = generateFood() // to make sure the food do not collide with the body of the snake
+    food = generateFood(); // to make sure the food do not collide with the body of the snake
     blocks[`${food.x}-${food.y}`].classList.add("food");
-
-    
 
     snake.unshift(head);
 
@@ -184,41 +182,31 @@ function restartGame() {
 }
 
 addEventListener("keydown", (event) => {
-
-   if (event.key === "ArrowUp") {
-    if(direction !== "down" || snake.length === 1)
-    direction = "up";
+  if (event.key === "ArrowUp") {
+    if (direction !== "down" || snake.length === 1) direction = "up";
   } else if (event.key === "ArrowLeft") {
-    if(direction !== "right" || snake.length === 1)
-    direction = "left";
+    if (direction !== "right" || snake.length === 1) direction = "left";
   } else if (event.key === "ArrowRight") {
-    if(direction !== "left" || snake.length === 1)
-    direction = "right";
+    if (direction !== "left" || snake.length === 1) direction = "right";
   } else if (event.key === "ArrowDown") {
-    if(direction !== "up" || snake.length === 1)
-    direction = "down";
+    if (direction !== "up" || snake.length === 1) direction = "down";
   }
 });
 
+function generateFood() {
+  let newFood;
+  let isOnSnake;
 
+  do {
+    newFood = {
+      x: Math.floor(Math.random() * rows),
+      y: Math.floor(Math.random() * cols),
+    };
 
-function generateFood(){
-    let  newFood;
-    let isOnSnake;
+    isOnSnake = snake.some((segment) => {
+      return newFood.x === segment.x && newFood.y === segment.y; // returns true or false
+    }); // if true then loop runs again and the random value generated again
+  } while (isOnSnake);
 
-    do {
-        newFood = {
-            x: Math.floor(Math.random() * rows),
-            y: Math.floor(Math.random() * cols),
-        };
-
-
-        isOnSnake = snake.some((segment) =>{
-            return newFood.x === segment.x && newFood.y === segment.y; // returns true or false
-        }) // if true then loop runs again and the random value generated again
-    } while(isOnSnake)
-
-        return newFood;
+  return newFood;
 }
-
-
